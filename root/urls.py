12f885f3615e2c9f3a4660dd8094fac1fd53d902
Mark Tang <mark@Marks-MacBook-Pro.local>
2014-09-09 22:16:09 +0800

    Init Django project

---
+++ root/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'root.views.home', name='home'),
+    url(r'^codebattle/', include('codebattle.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
